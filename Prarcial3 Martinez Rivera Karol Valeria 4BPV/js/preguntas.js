8e0d60938bca8d8053afa743d98b0ec31659a089
KarolValeria20 <martinez.rivera.karol.valeria.22@cetis156.edu.mx>
2024-06-04 01:18:46 -0600

    Add files via upload

---
+++ Prarcial3 Martinez Rivera Karol Valeria 4BPV/js/preguntas.js
@@ -0,0 +1,112 @@
+let questions = [{
+        numb: 1,
+        question: "¿Cual es el objetivo de las energías renovables?",
+        answer: "Reducir la contaminación y el uso de combustibles fósiles evitando la explotación de recursos no renovables.",
+        options: [
+            "Comprender la naturaleza y tratar de ordenar el amplio campo de los fenómenos ",
+            "Reducir la contaminación y el uso de combustibles fósiles evitando la explotación de recursos no renovables.",
+            "Proporcionar luz que se convierte en electricidad a través de paneles solares fotovoltaicos",
+            "Conocer las causas y las consecuencias de los comportamientos humanos."
+        ]
+    },
+    {
+        numb: 2,
+        question: " ¿Qué son las energías renovables?",
+        answer: "Energía que se obtiene a partir de fuentes naturales virtualmente inagotables.",
+        options: [
+            "Energía contenida en el núcleo de un átomo.",
+            "Energía que se obtiene a partir de fuentes naturales virtualmente inagotables.",
+            "Energía que se almacena en forma de compuesto químico en un portador de energía.",
+            "Forma de energía que se produce cuando un objeto, sustancia o partícula está en movimiento."
+        ]
+    },
+    {
+        numb: 3,
+        question: "¿Qué  significa fisión y fusión nuclear?",
+        answer: "La fisión nuclear es la reacción en la que el núcleo de un átomo pesado.",
+        options: [
+            "La fisión nuclear es la reacción en la que el núcleo de un átomo pesado.",
+            " La fisión nuclear es la reacción en la que el núcleo de un átomo pesado.",
+            "La fusión nuclear es una reacción nuclear en la que dos núcleos de átomos ligeros.",
+            "En la fusión nuclear, la energía se comprime cuando los núcleos de los átomos se combinan o se fusionan entre sí para formar un núcleo más grande."
+        ]
+    },
+    {
+        numb: 4,
+        question: "¿Qué es la energía nuclear?",
+        answer: "Es la energía contenida en el núcleo de un átomo.",
+        options: [
+            "Es la asociada a los cuerpos en movimiento y representa el esfuerzo que permite que un objeto pase del estado de reposo al de movimiento",
+            "Es la energía contenida en el núcleo de un átomo.",
+            "Es la suma de la energía cinética y la energía potencial de un sistema. ",
+            "Es aquella que se obtiene a partir de la fuerza del viento."
+        ]
+    },
+    {
+        numb: 5,
+        question: "¿Cuál es el objetivo de los coches eléctricos?",
+        answer: "Reducir el consumo de combustibles fósiles y las emisiones en el sector transporte.",
+        options: [
+            "Reducir el consumo de combustibles fósiles y las emisiones en el sector transporte.",
+            "Mayor porcentaje de contaminación.",
+            "Mas uso de combustible.",
+            "Choques."
+        ]
+    },
+    {
+        numb: 6,
+        question: "¿Por qué es mejor un vehículo eléctrico?",
+        answer: "Es el símbolo de una forma de movilidad más responsable y saludable para las personas y el entorno.",
+        options: [
+            "Porque utiliza gasolina.",
+            "Es el símbolo de una forma de movilidad más responsable y saludable para las personas y el entorno.",
+            "Porque gastas mas.",
+            "Lo recargas todo el tiempo."
+        ]
+    },
+    {
+        numb: 7,
+        question: "¿Qué es el cambio climático",
+        answer: "Es la alteración del clima y las temperaturas de la Tierra que afecta a los ecosistemas.",
+        options: [
+            "Es la alteración del clima y las temperaturas de la Tierra que afecta a los ecosistemas.",
+            "El cambio climático es una invención de los gobiernos para imponer más impuestos a la población.",
+            "El cambio climático es simplemente el clima cambiando de un día para otro, no hay nada de qué preocuparse.",
+            "El cambio climático es solo un ciclo natural de la Tierra, no tiene nada que ver con la actividad humana."
+        ]
+    },
+    {
+        numb: 8,
+        question: "¿Por qué el cambio climático nos preocupa tanto?",
+        answer: "Por que  aumenta la aparición de fenómenos meteorológicos más violentos, sequías, incendios, la muerte de especies animales y vegetales, los desbordamientos de ríos y lagos",
+        options: [
+            "Nos preocupa porque pensamos que las tecnologías futuras resolverán todos los problemas ambientales.",
+            "El cambio climático nos preocupa porque nos gusta preocuparnos por cosas que están fuera de nuestro control.",
+            "Por que  aumenta la aparición de fenómenos meteorológicos más violentos, sequías, incendios, la muerte de especies animales y vegetales, los desbordamientos de ríos y lagos",
+            "Nos preocupa porque es una moda social seguir la corriente y hablar sobre el cambio climático."
+        ]
+    },
+    {
+        numb: 9,
+        question: "¿Cuáles son los tipos de innovaciones tecnológicas que hay?",
+        answer: "Innovación tecnológica disruptiva, incremental y radical.",
+        options: [
+            "Innovación tecnológica incremental, estratégica y coloquial.",
+            "innovación tecnológica de productos y servicios.",
+            "Innovación tecnológica disruptiva, incremental y radical.",
+            "Innovación tecnológica disruptiva, producción y estratégica."
+        ]
+    },
+    {
+        numb: 10,
+        question: "¿Qué es la innovación tecnológica?",
+        answer: "Utilizar el conocimiento para crear productos, servicios o procesos que son nuevos para la empresa, o en su defecto mejorar los que ya existen.",
+        options: [
+            "Utilizar el conocimiento para crear productos, servicios o procesos que son nuevos para la empresa, o en su defecto mejorar los que ya existen.",
+            "Utilizar el conocimiento para hacer cosas.",
+            "innovar.",
+            "Conjunto de teorías y de técnicas que permiten el aprovechamiento práctico del conocimiento científico."
+        ]
+    },
+
+];
